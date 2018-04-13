697fd38579a220467feea78e4636feaf25680614
Norbert <nkozera@gmail.com>
2018-04-13 22:29:12 +0200

    created logout, email/password login/register, anonymus login

--- app/src/main/java/pl/nkozera/mastersthesis/FindCityActivity.kt
+++ app/src/main/java/pl/nkozera/mastersthesis/FindCityActivity.kt
@@ -6,14 +6,56 @@
 
 package pl.nkozera.mastersthesis
 
+import android.content.Intent
 import android.os.Bundle
+import android.service.carrier.CarrierMessagingService
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import org.w3c.dom.Text
+import android.support.annotation.NonNull
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.Status
+
 
 class FindCityActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_your_city)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    private lateinit var mAuth: FirebaseAuth
+
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth.currentUser
+        val textVIew: TextView = findViewById<View>(R.id.textView) as TextView
+        var username = currentUser?.displayName
+        if(username.isNullOrEmpty()) {
+            textVIew.text = currentUser?.email
+        } else {
+            textVIew.text = currentUser?.displayName
+        }
+
+
+    }
+
+
+    fun firebaseLogOut(view: View){
+        mAuth.signOut()
+        Toast.makeText(this, mAuth.currentUser?.toString(), Toast.LENGTH_LONG).show()
+
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 
 }
--- app/src/main/java/pl/nkozera/mastersthesis/LoginActivity.kt
+++ app/src/main/java/pl/nkozera/mastersthesis/LoginActivity.kt
@@ -10,7 +10,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Toast
 import com.google.android.gms.auth.api.signin.*
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
@@ -18,13 +17,11 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import org.slf4j.LoggerFactory
 import com.google.firebase.auth.FirebaseUser
-import android.support.design.widget.Snackbar
-import com.google.firebase.auth.AuthResult
-import android.support.annotation.NonNull
-import com.google.android.gms.tasks.OnCompleteListener
+import android.util.Log
+import android.widget.*
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.AuthCredential
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity : AppCompatActivity() {
@@ -32,7 +29,7 @@ class LoginActivity : AppCompatActivity() {
 
     lateinit var mGoogleSignInCLient: GoogleSignInClient
     lateinit var googleSignInOptions: GoogleSignInOptions
-    val RC_SIGN_IN: Int = 1;
+    val RC_SIGN_IN: Int = 1
     private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,28 +50,119 @@ class LoginActivity : AppCompatActivity() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = mAuth.currentUser
+
         updateUI(currentUser)
     }
 
     fun emailSignInOnClick(view: View) {
-        LOG.info("Logger.info")
-        LOG.debug("Logger.debug")
-        LOG.warn("Logger.warn")
-        LOG.error("Logger.error")
-        LOG.trace("Logger.trace")
-        println("test")
+        val userEmail: String = email.text.toString()
+        val passwd: String = password.text.toString()
+
+
+        if (validateEmailAndPasswd(userEmail, passwd)) {
+            firebaseAuthWithPassword(userEmail, passwd)
+        }
+    }
+
+    private fun validateEmailAndPasswd(userEmail: String, passwd: String): Boolean {
+        return if (userEmail.isEmpty()) {
+            email.error = getString(R.string.error_field_is_required)
+            false
+        } else if (passwd.isEmpty()) {
+            password.error = getString(R.string.error_field_is_required)
+            false
+        } else if (!userEmail.contains("@") &&
+                !userEmail.substring(userEmail.indexOf("@")).contains(".")) {
+            email.error = getString(R.string.error_invalid_email)
+            false
+        } else {
+            true
+        }
+    }
+
+
+    fun emailRegisterOnClick(view: View) {
+
+        val emailSignInButtonParams = email_sign_in_button.layoutParams as RelativeLayout.LayoutParams
+        val emailSignUpButtonParams = email_register_button.layoutParams as RelativeLayout.LayoutParams
+
+        emailSignInButtonParams.addRule(RelativeLayout.BELOW, R.id.email_register_account_button)
+        emailSignUpButtonParams.addRule(RelativeLayout.BELOW, R.id.email_register_account_button)
+
+        view.layoutParams = emailSignInButtonParams
+        view.layoutParams = emailSignUpButtonParams
+        email_register_button.text = getString(R.string.cancel)
+        email_register_button.setOnClickListener { cancelRegistration(view, emailSignInButtonParams, emailSignUpButtonParams) }
+
+        retype_new_password.visibility = View.VISIBLE
+        checkboxRestaurantOwner.visibility = View.VISIBLE
+        email_register_account_button.visibility = View.VISIBLE
+
 
     }
+
+    fun confirmSignUp(view: View) {
+        val userEmail: String = email.text.toString()
+        val passwd: String = password.text.toString()
+        val retypePasswd: String = retype_new_password.text.toString()
+        if (validateEmailAndPass(userEmail, passwd, retypePasswd)) {
+            firebaseRegisteWithPassword(userEmail, passwd)
+        }
+    }
+
+    private fun cancelRegistration(view: View, emailSignIn: RelativeLayout.LayoutParams, emailSignUp: RelativeLayout.LayoutParams) {
+
+        retype_new_password.visibility = View.INVISIBLE
+        checkboxRestaurantOwner.visibility = View.INVISIBLE
+        email_register_account_button.visibility = View.INVISIBLE
+
+        emailSignIn.addRule(RelativeLayout.BELOW, R.id.password)
+        emailSignUp.addRule(RelativeLayout.BELOW, R.id.password)
+
+
+        view.layoutParams = emailSignIn
+        view.layoutParams = emailSignUp
+
+        email_register_button.text = getString(R.string.action_register)
+        email_register_button.setOnClickListener { emailRegisterOnClick(view) }
+
+    }
+
+    private fun validateEmailAndPass(userEmail: String, passwd: String, retypePasswd: String): Boolean {
+        return if (validateEmailAndPasswd(userEmail,passwd)) {
+            when {
+                passwd.length < 8 -> {
+                    password.error = getString(R.string.error_incorrect_password)
+                    false
+                }
+                retypePasswd.length < 8 -> {
+                    retype_new_password.error = getString(R.string.error_incorrect_password)
+                    false
+                }
+                retypePasswd != passwd ->{
+                    retype_new_password.error = getString(R.string.error_different_passwords)
+                    false
+                }
+                else -> true
+            }
+        } else {
+            false
+        }
+
+    }
+
 
     fun googleSignInOnClick() {
         LOG.info("Clicking google sign in button")
         println("test")
+
         googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .requestId()
                 .build()
         mGoogleSignInCLient = GoogleSignIn.getClient(this, googleSignInOptions)
+        mGoogleSignInCLient.signOut()
         val signInIntent: Intent = mGoogleSignInCLient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
@@ -92,7 +180,6 @@ class LoginActivity : AppCompatActivity() {
         if (user != null) {
             startActivity(Intent(this, FindCityActivity::class.java))
         } else {
-            Toast.makeText(this, "Problem while logging in", Toast.LENGTH_LONG).show()
         }
     }
 
@@ -108,9 +195,11 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+
     private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
 
         val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        Log.i("dupa", "jaś")
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
@@ -119,8 +208,66 @@ class LoginActivity : AppCompatActivity() {
                         updateUI(user)
                     } else {
                         // If sign in fails, display a message to the user.
-                        Toast.makeText(this, "Problem while logging in", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this, "Problem while logging in 2", Toast.LENGTH_LONG).show()
                     }
                 }
     }
+
+    private fun firebaseRegisteWithPassword(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+
+
+                    if (task.isSuccessful) {
+
+                        val user = mAuth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "Problem while registrating", Toast.LENGTH_LONG).show()
+                    }
+
+                }
+    }
+
+    private fun firebaseAuthWithPassword(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+
+
+                    if (task.isSuccessful) {
+
+                        val user = mAuth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "Problem while logging", Toast.LENGTH_LONG).show()
+                    }
+
+                }
+    }
+    fun firebaseAuthAnonymusly(view: View) {
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+
+
+                    if (task.isSuccessful) {
+
+                        val user = mAuth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "Problem while logging", Toast.LENGTH_LONG).show()
+                    }
+
+                }
+    }
+
+
+    fun firebaseLogOut(view: View) {
+        mAuth.signOut()
+        mGoogleSignInCLient.signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
 }
+
